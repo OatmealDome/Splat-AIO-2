ff498e6d1ed24a8be02654550f1ebd4c5ce6e129
OatmealDome <theoatmealdome@gmail.com>
2016-08-28 13:29:35 -0400

    Add the Configuration class (*facepalm*)

---
+++ SplatAIO/Configuration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SplatAIO
+{
+    [Serializable]
+    public class Configuration
+    {
+        public int version;
+        public String lastIp;
+        public bool allowStatistics;
+
+        private static XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+        public static Configuration currentConfig;
+    
+        public static void Load()
+        {
+            if (!File.Exists("Configuration.xml"))
+            {
+                AllowStatisticsForm allowStatisticsForm = new AllowStatisticsForm();
+                allowStatisticsForm.ShowDialog();
+
+                currentConfig = new Configuration();
+                currentConfig.version = Form1.ver;
+                currentConfig.lastIp = "";
+                currentConfig.allowStatistics = allowStatisticsForm.allowCollection;
+
+                Save();
+            }
+            else
+            {
+                using (FileStream stream = File.OpenRead("Configuration.xml"))
+                {
+                    currentConfig = (Configuration)serializer.Deserialize(stream);
+                }
+            }
+        }
+
+        public static void Save()
+        {
+            File.Delete("Configuration.xml");
+            using (FileStream writer = File.OpenWrite("Configuration.xml"))
+            {
+                serializer.Serialize(writer, currentConfig);
+            }
+        }
+
+    }
+}
